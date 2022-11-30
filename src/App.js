d4f6c41166a2be20d67440f941ea7b92d515d78c
Faith-vaati <vaati.faith79@gmail.com>
2022-11-30 11:53:57 +0300

    Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React from "react";
+import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
 import "./App.scss";
 
 //Importing Pages
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import CreateUsers from "./pages/CreateUsers";
+import Dashboard from "./pages/Dashboard";
+import UserProfile from "./pages/UserProfile";
+import Users from "./pages/Users";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header"></div>
-      <Header />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/create" element={<CreateUsers />} />
+        <Route exact path="/profile" element={<UserProfile />} />
+        <Route exact path="/users" element={<Users />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+ 
+
+export default function Dashboard(){
+    return (
+    <div className="dashboard">
+        <p>I am a Dashboard</p>
+    </div>
+    );
+}
