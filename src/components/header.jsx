e9b3f53a0ae8eb6bf3485969a82be30d89365488
Faith-vaati <vaati.faith79@gmail.com>
2022-12-08 17:33:17 +0300

    Changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { useRef } from "react";
 
 
 
 export default function Header(){
+   const rfSearch = useRef();
+
+
     return(
         <div className="header">
             <Navbar />
             <div className="top">
             <div className="search">
-            <input type="textarea">
+            <input ref={rfSearch} type="textarea">
             </input>
             <i class="fa fa-search" aria-hidden="true"></i>
             </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/pages.scss"
+import { useState} from "react";
 
 export default function Dashboard(){
+const [test, setTest] = useState('Click Me');
+
+function afterClicking(){
+    setTest('I am a Button');
+}
+
     return (
     <div className="dashboard">
         <Header />
         <p>I am a Dashboard</p>
+        <div className="statetest">
+            <button onClick={afterClicking}>{test}</button>
+        </div>
     </div>
     );
 }
