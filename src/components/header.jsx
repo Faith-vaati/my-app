31c9cd6535d09563cae3563d265a2dbd62f160aa
Faith-vaati <vaati.faith79@gmail.com>
2022-12-08 15:02:32 +0300

    Changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,14 +7,16 @@ export default function Header(){
     return(
         <div className="header">
             <Navbar />
+            <div className="top">
             <div className="search">
             <input type="textarea">
             </input>
             <i class="fa fa-search" aria-hidden="true"></i>
             </div>
-            {/* <div className="profile">
-            <i className="fa-solid fa-user"></i>
-            </div> */}
+            <div className="profile">
+            <i class="fa fa-user-circle" aria-hidden="true"></i>
+            </div>
+            </div>
         </div>
     );
 }
