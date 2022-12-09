9165275c786f3ce3aa77e43de219a9259977a1e2
Faith-vaati <vaati.faith79@gmail.com>
2022-12-09 17:23:30 +0300

    Changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -65,6 +65,28 @@ export default function Dashboard(){
         </table>
       );
     }
+
+    function SearchBar(){
+        return(
+        <form>
+            <input type="text" placeholder= "Search"/>
+            <label>
+                <input type="checkbox" />
+                Only Show Products in Stock
+                {' '}
+            </label>
+        </form>
+        );
+    }
+
+    function FilterableProductTable(products){
+        return(
+            <div>
+                <SearchBar/>
+                <ProductTable products={products}/>
+            </div>
+        );
+    }
     
     return (
     <div className="dashboard">
