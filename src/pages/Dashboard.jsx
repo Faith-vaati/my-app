5da888abae543967d4938a61c79d121c57a8c96a
Faith-vaati <vaati.faith79@gmail.com>
2022-12-09 17:40:48 +0300

    Changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -87,13 +87,20 @@ export default function Dashboard(){
             </div>
         );
     }
-    
+    const PRODUCTS = [
+        {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+        {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+        {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+        {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+        {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+        {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+      ];
     return (
     <div className="dashboard">
         <Header />
         <p>I am a Dashboard</p>
         <div className="statetest">
-            
+        <FilterableProductTable products={PRODUCTS} />
         </div>
     </div>
     );
